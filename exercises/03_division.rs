e5b43eeec402e03a9687268fda1e08e09db94e38
Johannes Hayeß <jhaye@mailbox.org>
2022-09-17 01:20:14 +0200

    Add solution to ex. 03

--- exercises/03_division.rs
+++ exercises/03_division.rs
@@ -2,6 +2,23 @@ extern crate creusot_contracts;
 use creusot_contracts::*;
 
 // Implement and prove the Euclidean division
-fn divide(u: i64, b: i64) -> i64 {
-    todo!()
+
+#[requires(@b != 0)]
+#[requires(@a >= 0 && @b >= 0)]
+#[requires(@a >= @b)]
+#[ensures(exists<r: Int> @a == (@b * @result) + r && 0 <= r && r < @b)]
+#[ensures(@result >= 0)]
+fn divide(a: i64, b: i64) -> i64 {
+    let mut r = a;
+    let mut q = 0;
+
+    #[invariant(reverse, a == (b * q) + r)]
+    #[invariant(q_bounds, @q >= 0 && q <= a)]
+    #[invariant(r_bounds, @r >= 0 && q <= a)]
+    while r >= b {
+        r -= b;
+        q += 1;
+    }
+
+    q
 }
